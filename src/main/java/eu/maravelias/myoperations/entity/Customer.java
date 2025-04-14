ed656336ddacf6cd1f0bb56f14f60efb98152e5a
Giorgos Maravelias <maravelias@netmechanics.gr>
2025-04-14 16:16:58 +0300

    Add CustomerDetail view and Contact entity
    Introduced `CustomerDetailView` for managing customer details and implemented the `Contact` entity to support customer contacts. Updated the `Customer` entity to include a one-to-many relationship with `Contact`. Adjusted localization and UI files accordingly.

--- src/main/java/eu/maravelias/myoperations/entity/Customer.java
+++ src/main/java/eu/maravelias/myoperations/entity/Customer.java
@@ -11,63 +11,75 @@
     * See the License for the specific language governing permissions and
     * limitations under the License.
     */
-package eu.maravelias.myoperations.entity;
+   package eu.maravelias.myoperations.entity;
 
-import java.time.OffsetDateTime;
-import java.util.UUID;
+   import java.time.OffsetDateTime;
+   import java.util.List;
+   import java.util.UUID;
 
-import io.jmix.core.annotation.DeletedBy;
-import io.jmix.core.annotation.DeletedDate;
-import io.jmix.core.entity.annotation.JmixGeneratedValue;
-import io.jmix.core.metamodel.annotation.JmixEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Index;
-import jakarta.persistence.Table;
-import jakarta.persistence.Version;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotNull;
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
+   import io.jmix.core.DeletePolicy;
+   import io.jmix.core.annotation.DeletedBy;
+   import io.jmix.core.annotation.DeletedDate;
+   import io.jmix.core.entity.annotation.JmixGeneratedValue;
+   import io.jmix.core.entity.annotation.OnDelete;
+   import io.jmix.core.metamodel.annotation.Composition;
+   import io.jmix.core.metamodel.annotation.JmixEntity;
+   import jakarta.persistence.Column;
+   import jakarta.persistence.Entity;
+   import jakarta.persistence.Id;
+   import jakarta.persistence.Index;
+   import jakarta.persistence.OneToMany;
+   import jakarta.persistence.OrderBy;
+   import jakarta.persistence.Table;
+   import jakarta.persistence.Version;
+   import jakarta.validation.constraints.Email;
+   import jakarta.validation.constraints.NotNull;
+   import org.springframework.data.annotation.CreatedBy;
+   import org.springframework.data.annotation.CreatedDate;
+   import org.springframework.data.annotation.LastModifiedBy;
+   import org.springframework.data.annotation.LastModifiedDate;
 
-@JmixEntity
-@Table(name = "MYOPS_CUSTOMER", indexes = {
-    @Index(name = "IDX_MYOPS_CUSTOMER", columnList = "ACTIVE")
-})
-@Entity(name = "myops_Customer")
-public class Customer {
-    @JmixGeneratedValue @Column(name = "ID", nullable = false) @Id private UUID id;
+   @JmixEntity
+   @Table(name = "MYOPS_CUSTOMER", indexes = {
+       @Index(name = "IDX_MYOPS_CUSTOMER", columnList = "ACTIVE")
+   })
+   @Entity(name = "myops_Customer")
+   public class Customer {
+       @JmixGeneratedValue @Column(name = "ID", nullable = false) @Id private UUID id;
 
-    @Column(name = "ZIP") private String zip;
+       @OnDelete(DeletePolicy.CASCADE) @Composition @OrderBy("lastName DESC") @OneToMany(mappedBy = "customer") private List<Contact> contacts;
 
-    @Column(name = "ADDRESS") private String address;
+       @Column(name = "ZIP") private String zip;
 
-    @Column(name = "VAT", length = 20) private String vat;
+       @Column(name = "ADDRESS") private String address;
 
-    @Column(name = "CITY") private String city;
+       @Column(name = "VAT", length = 20) private String vat;
 
-    @NotNull @Column(name = "COMPANY_NAME", nullable = false) private String companyName;
+       @Column(name = "CITY") private String city;
 
-    @Email(message = "{msg://eu.maravelias.myoperations.entity/Customer.email.validation.Email}") @Column(name = "EMAIL") private String email;
+       @NotNull @Column(name = "COMPANY_NAME", nullable = false) private String companyName;
 
-    @Column(name = "ACTIVE", nullable = false) @NotNull private Boolean active = false;
+       @Email(message = "{msg://eu.maravelias.myoperations.entity/Customer.email.validation.Email}") @Column(name = "EMAIL") private String email;
 
-    @Column(name = "VERSION", nullable = false) @Version private Integer version;
+       @Column(name = "ACTIVE", nullable = false) @NotNull private Boolean active = false;
 
-    @CreatedBy @Column(name = "CREATED_BY") private String createdBy;
+       @Column(name = "VERSION", nullable = false) @Version private Integer version;
 
-    @CreatedDate @Column(name = "CREATED_DATE") private OffsetDateTime createdDate;
+       @CreatedBy @Column(name = "CREATED_BY") private String createdBy;
 
-    @LastModifiedBy @Column(name = "LAST_MODIFIED_BY") private String lastModifiedBy;
+       @CreatedDate @Column(name = "CREATED_DATE") private OffsetDateTime createdDate;
 
-    @LastModifiedDate @Column(name = "LAST_MODIFIED_DATE") private OffsetDateTime lastModifiedDate;
+       @LastModifiedBy @Column(name = "LAST_MODIFIED_BY") private String lastModifiedBy;
 
-    @DeletedBy @Column(name = "DELETED_BY") private String deletedBy;
+       @LastModifiedDate @Column(name = "LAST_MODIFIED_DATE") private OffsetDateTime lastModifiedDate;
 
-    @DeletedDate @Column(name = "DELETED_DATE") private OffsetDateTime deletedDate;
+       @DeletedBy @Column(name = "DELETED_BY") private String deletedBy;
+
+       @DeletedDate @Column(name = "DELETED_DATE") private OffsetDateTime deletedDate;
+
+       public List<Contact> getContacts() {return contacts;}
+
+    public void setContacts(List<Contact> contacts) {this.contacts = contacts;}
 
     public Boolean getActive() {return active;}
 
